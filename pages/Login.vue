970baea747776a830dea42780d81fce94f69dea6
dre1597 <74803456+dre1597@users.noreply.github.com>
2021-05-11 01:28:54 -0300

    html inicial da página de login e da página de cadastro

---
+++ pages/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="login">
+        <form action="">
+            <fieldset>
+                <label for="email">Email</label>
+                <input id="email" type="email" required>
+            </fieldset>
+            <fieldset>
+                <label for="senha">Senha</label>
+                <input id="senha" type="password" pattern="" required>
+            </fieldset>
+            <fieldset>
+                <input id="checkbox" type="checkbox" checked>
+                <label for="checkbox">Continuar conectado?</label>
+            </fieldset>
+            <a href="#">Esqueceu sua senha?</a>
+            <a href="#">Criar uma conta</a>
+            <button type="submit">Entrar</button>
+        </form>
+    </div>
+</template>
---
+++ pages/SignIn.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="sign-in">
+        <form action="">
+            <fieldset>
+                <label for="nome">Nome</label>
+                <input id="nome" type="text" required>
+            </fieldset>
+            <fieldset>
+                <label for="email">Email</label>
+                <input id="email" type="email" required>
+            </fieldset>
+            <fieldset>
+                <label for="senha">Senha</label>
+                <input id="senha" type="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,12}$" required>
+            </fieldset>
+            <fieldset>
+                <label for="confirmar-senha">Confirmar senha</label>
+                <input id="confirmar-senha" type="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,12}$" required>
+            </fieldset>
+            <fieldset>
+                <input id="checkbox" type="checkbox" checked>
+                <label for="checkbox">Aceito receber e-mails com as novidades.</label>
+            </fieldset>
+            <button type="submit">Cadastrar</button>
+        </form>
+    </div>
+</template>
